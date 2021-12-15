8ca670a86c83fbf61eaab959b3c34f5b3e1c6b1a
OrcaIt <whitefox@kakao.com>
2021-12-15 14:28:01 +0900

    Signed-off-by: OrcaIt <whitefox@kakao.com>

---
+++ Web/JS/signin.js
@@ -0,0 +1,5 @@
+document.getElementById("okButton")
+    .addEventListener("click", function() {
+        document.getElementById("main1").hidden = true;
+        document.getElementById("main2").hidden = false;
+    }, false);
