63d6f24cb7baa1ef0bfc2e7ee8a7b2aa3ea7d9ed
akwjr963 <akwjr963@daum.net>
2021-12-21 13:34:52 +0900

    황성민 : ResetPW.html/css 1차수정 및 js생성

---
+++ Web/JS/ResetPW.js
@@ -0,0 +1,5 @@
+document.getElementById("okButton")
+    .addEventListener("click", function() {
+        document.getElementById("checkID").hidden = true;
+        document.getElementById("ResetPW").hidden = false;
+    }, false);
