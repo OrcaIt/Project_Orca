4ed1b778c5845c3fe712e8208081e1ac4a335755
akwjr963 <akwjr963@daum.net>
2021-12-21 14:56:01 +0900

    황성민 : ResetPW.html 2차수정(이메일인증div생성 및 css, js수정)

--- Web/JS/ResetPW.js
+++ Web/JS/ResetPW.js
@@ -1,5 +1,11 @@
 document.getElementById("okButton")
     .addEventListener("click", function() {
         document.getElementById("checkID").hidden = true;
+        document.getElementById("CheckE-Mail").hidden = false;;
+    }, false);
+
+document.getElementById("Next")
+    .addEventListener("click", function() {
+        document.getElementById("CheckE-Mail").hidden = true;
         document.getElementById("ResetPW").hidden = false;
     }, false);
