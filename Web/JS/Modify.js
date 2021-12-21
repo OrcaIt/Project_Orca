83d6fe3f5c034c53405468b942822ea86e8886bc
akwjr963 <akwjr963@daum.net>
2021-12-21 17:40:32 +0900

    황성민 : Modify.html,css,js파일 생성(추가 수정 예정)

---
+++ Web/JS/Modify.js
@@ -0,0 +1,5 @@
+document.getElementById("okButton")
+    .addEventListener("click", function() {
+        document.getElementById("main").hidden = true;
+        document.getElementById("Modify").hidden = false;;
+    }, false);
