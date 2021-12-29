568bb7d45dc0c338d55a96f4b68f6daf46936521
akwjr963 <akwjr963@daum.net>
2021-12-29 14:27:36 +0900

    황성민 Test.html,css 생성(modify.html,css 복사 및 수정용)

--- Web/JS/Modify.js
+++ Web/JS/Modify.js
@@ -1,5 +1,5 @@
 document.getElementById("okButton")
     .addEventListener("click", function() {
-        document.getElementById("main").hidden = true;
+        document.getElementById("explain").hidden = true;
         document.getElementById("Modify").hidden = false;;
     }, false);
